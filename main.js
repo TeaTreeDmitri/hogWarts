f388bdfa26a9db9dd5d314beaf55ff419da8a742
Gareth Davies <mrgarethddavies@gmail.com>
2022-09-15 23:22:09 +0200

    init

---
+++ main.js
@@ -0,0 +1,46 @@
+"use strict"
+
+//global variables
+
+
+//global objects
+
+
+//fetch the data 
+
+    //fetch names 
+
+    //fetch images
+
+    //fetch blood status
+
+    //fetch house information
+
+
+//clean up the data 
+
+
+//display the data 
+
+
+//filter the data 
+
+
+//sort the data
+
+
+//open information page 
+
+
+//display student information
+
+
+//expel function
+
+
+//prefect function
+
+
+//hack the system / hackTheSystem()
+
+
