87b21afbee2b81a05c0bbb06e5277bb5c4bc5ffa
Gareth Davies <mrgarethddavies@gmail.com>
2022-09-15 23:40:07 +0200

    main structural outline

--- main.js
+++ main.js
@@ -25,20 +25,42 @@
 
 //filter the data 
 
+    //filter by attending/expelled
+
+    //filter by house
+
+    //filter by prefects
+
+    //filter by inquisitors
+
 
 //sort the data
 
+    //sort by first name
 
-//open information page 
+    //sort by last name
+
+    //sort by house
+
+
+//open student page 
 
 
 //display student information
 
 
 //expel function
+    
+    //configure pop up
+
+    //display pop up
 
 
 //prefect function
+    
+    //configure pop up
+
+    //display pop up
 
 
 //hack the system / hackTheSystem()
