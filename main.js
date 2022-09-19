77370c97e420832013601a2baaf0e529fd7fa016
Gareth Davies <mrgarethddavies@gmail.com>
2022-09-19 18:41:44 +0200

    sorting bidirectional with toggle

--- main.js
+++ main.js
@@ -12,22 +12,22 @@ let imageTag2;
 //global objects
 let allStudents = [];
 
-    // create template for student data
+// create template for student data
 
-    const Student = {
-            firstName: "",
-            middleName: "",
-            lastName: "",
-            nickName: "",
-            studentGender: "",
-            studentImg1: "",
-            studentImg2: "",
-            houseName: "",
-            bloodType: "",
-            isPrefect: "",
-            isInquisitor: "",
-            isExpelled: "",
-    }
+const Student = {
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    nickName: "",
+    studentGender: "",
+    studentImg1: "",
+    studentImg2: "",
+    houseName: "",
+    bloodType: "",
+    isPrefect: "",
+    isInquisitor: "",
+    isExpelled: "",
+}
 
 
 
@@ -49,18 +49,18 @@ async function initialise() {
 
 function addButtons() {
     document.querySelectorAll("[data-action='filter']")
-    .forEach( button => button.addEventListener("click", selectFilter))
+        .forEach(button => button.addEventListener("click", selectFilter))
 
     document.querySelectorAll("[data-action='sort']")
-    .forEach( button => button.addEventListener("click", selectSort))
+        .forEach(button => button.addEventListener("click", selectSort))
 }
 
 //fetch names 
-async function loadNames(){
+async function loadNames() {
     const nameData = await fetch("https://petlatkea.dk/2021/hogwarts/students.json");
     studentNames = await nameData.json();
     prepareObjects(studentNames);
-    
+
 }
 
 //fetch images
@@ -80,92 +80,92 @@ function prepareObjects(names) {
 
 //clean up the name data 
 function prepareData(el) {
-        // trim whitespace from edges
-        el.fullname = el.fullname.trim();
+    // trim whitespace from edges
+    el.fullname = el.fullname.trim();
 
-        //make all letters lower case
-        el.fullname = el.fullname.toLowerCase();
+    //make all letters lower case
+    el.fullname = el.fullname.toLowerCase();
 
-        //return nickname betweens ""
-        let nickName = el.fullname.substring(el.fullname.indexOf(`"`), el.fullname.lastIndexOf(`"`) + 1)
+    //return nickname betweens ""
+    let nickName = el.fullname.substring(el.fullname.indexOf(`"`), el.fullname.lastIndexOf(`"`) + 1)
 
-        //remove nickname from fullname
-        el.fullname = el.fullname.replace(nickName, "");
+    //remove nickname from fullname
+    el.fullname = el.fullname.replace(nickName, "");
 
-        //tidy up nickname
-        nickName = nickName.replaceAll(`"`, "");
-        nickName = nickName.charAt(0).toUpperCase() + nickName.slice(1);
+    //tidy up nickname
+    nickName = nickName.replaceAll(`"`, "");
+    nickName = nickName.charAt(0).toUpperCase() + nickName.slice(1);
 
-        //replace double " " with " ";
-        el.fullname = el.fullname.replaceAll("  ", " ");
+    //replace double " " with " ";
+    el.fullname = el.fullname.replaceAll("  ", " ");
 
-        //capitalise first letter and first letter after " ";
-        let fullNameArr = el.fullname.split(" ");
+    //capitalise first letter and first letter after " ";
+    let fullNameArr = el.fullname.split(" ");
 
-        for (let i = 0; i < fullNameArr.length; i++) {
-            fullNameArr[i] = fullNameArr[i][0].toUpperCase() + fullNameArr[i].substring(1);
-        }
+    for (let i = 0; i < fullNameArr.length; i++) {
+        fullNameArr[i] = fullNameArr[i][0].toUpperCase() + fullNameArr[i].substring(1);
+    }
 
-        el.fullname = fullNameArr.join(" ");
+    el.fullname = fullNameArr.join(" ");
 
-        //capitalise first letter after "-";
-        let fullNameArr2 = el.fullname.split("-");
+    //capitalise first letter after "-";
+    let fullNameArr2 = el.fullname.split("-");
 
-        for (let i = 0; i < fullNameArr2.length; i++) {
-            fullNameArr2[i] = fullNameArr2[i][0].toUpperCase() + fullNameArr2[i].substring(1);
-        }
+    for (let i = 0; i < fullNameArr2.length; i++) {
+        fullNameArr2[i] = fullNameArr2[i][0].toUpperCase() + fullNameArr2[i].substring(1);
+    }
 
-        el.fullname = fullNameArr2.join("-");
+    el.fullname = fullNameArr2.join("-");
 
-        //trim white space from start
-        el.fullname = el.fullname.trimStart();
+    //trim white space from start
+    el.fullname = el.fullname.trimStart();
 
-        //tidy up house name 
-        el.house = el.house.replaceAll(`"`, "");
-        let houseName = el.house.toLowerCase();
-        houseName = houseName.trim();
-        houseName = houseName.charAt(0).toUpperCase() + houseName.slice(1);
+    //tidy up house name 
+    el.house = el.house.replaceAll(`"`, "");
+    let houseName = el.house.toLowerCase();
+    houseName = houseName.trim();
+    houseName = houseName.charAt(0).toUpperCase() + houseName.slice(1);
 
-        //tidy up gender
-        let studentGender = el.gender.toLowerCase();
-        studentGender = studentGender.trim();
-        studentGender = studentGender.charAt(0).toUpperCase() + studentGender.slice(1);
+    //tidy up gender
+    let studentGender = el.gender.toLowerCase();
+    studentGender = studentGender.trim();
+    studentGender = studentGender.charAt(0).toUpperCase() + studentGender.slice(1);
 
-        
-        //Separate names and add to variables
-        let fullNameArr3 = el.fullname.split(" ");
-        if (fullNameArr3.length > 2) {
-            firstName = fullNameArr3[0];
-            middleName = fullNameArr3[1];
-            lastName = fullNameArr3[2];
-        } else if (fullNameArr.length <= 2) {
-            firstName = fullNameArr3[0];
-            middleName = "null";
-            lastName = fullNameArr3[1];
-        } else if (fullNameArr3.length < 2) {
-            firstName = fullNameArr3[0];
-            middleName = "null"
-            lastName = "null"
-        };
-        // console.log("Fullname: ", el.fullname);
-        // console.log("First Name: ", firstName);
-        // console.log("Middle Name: ", middleName);
-        // console.log("Last Name: ", lastName);
-        // console.log("Nickname: ", nickName);
-        // console.log("House: ", houseName);
-        // console.log("Gender: ", studentGender);
-        // console.log(" ");
 
-        //put variables into newobjects
-        const student = Object.create(Student);
-        student.firstName = firstName;
-        student.lastName = lastName;
-        student.middleName = middleName;
-        student.nickName = nickName;
-        student.house = houseName;
-        student.gender = studentGender;
+    //Separate names and add to variables
+    let fullNameArr3 = el.fullname.split(" ");
+    if (fullNameArr3.length > 2) {
+        firstName = fullNameArr3[0];
+        middleName = fullNameArr3[1];
+        lastName = fullNameArr3[2];
+    } else if (fullNameArr.length <= 2) {
+        firstName = fullNameArr3[0];
+        middleName = "null";
+        lastName = fullNameArr3[1];
+    } else if (fullNameArr3.length < 2) {
+        firstName = fullNameArr3[0];
+        middleName = "null"
+        lastName = "null"
+    };
+    // console.log("Fullname: ", el.fullname);
+    // console.log("First Name: ", firstName);
+    // console.log("Middle Name: ", middleName);
+    // console.log("Last Name: ", lastName);
+    // console.log("Nickname: ", nickName);
+    // console.log("House: ", houseName);
+    // console.log("Gender: ", studentGender);
+    // console.log(" ");
 
-        return student;
+    //put variables into newobjects
+    const student = Object.create(Student);
+    student.firstName = firstName;
+    student.lastName = lastName;
+    student.middleName = middleName;
+    student.nickName = nickName;
+    student.house = houseName;
+    student.gender = studentGender;
+
+    return student;
 }
 
 //filter the data 
@@ -185,7 +185,7 @@ function filterList(filterBy) {
         filteredList = allStudents.filter(isRave);
     } else if (filterBy === "Hufflepuff") {
         filteredList = allStudents.filter(isHuff);
-    } 
+    }
 
     displayList(filteredList)
 }
@@ -217,50 +217,42 @@ function isHuff(item) {
 //sort the data
 function selectSort(event) {
     const sortBy = event.target.dataset.sort;
-    console.log("User selected: ", sortBy);
-    sortList(sortBy);
+    const sortDir = event.target.dataset.sortDirection;
+
+    //toggle direction
+    if (sortDir === "asc") {
+        event.target.dataset.sortDirection = "desc"
+    } else {
+        event.target.dataset.sortDirection = "asc"
+    }
+    console.log("User selected: ", sortBy, sortDir);
+    sortList(sortBy, sortDir);
 }
 
-
-function sortList(sortBy) {
+//sort by first name / last name / house
+function sortList(sortBy, sortDir) {
     let sortedList = allStudents;
-    if (sortBy === "first") {
-        sortedList = sortedList.sort(sortByFirst)
-    } else if (sortBy === "last") {
-        sortedList = sortedList.sort(sortByLast)
-    } else if (sortBy === "house") {
-        sortedList = sortedList.sort(sortByHouse)
+    let direction;
+    if (sortDir === "desc") {
+        direction = 1
+    } else {
+        direction = -1;
+    }
+
+
+    sortedList = sortedList.sort(sortByProperty)
+
+    function sortByProperty(studentA, studentB) {
+        if (studentA[sortBy] < studentB[sortBy]) {
+            return -1 * direction;
+        } else {
+            return 1 * direction;
+        }
     }
 
     displayList(sortedList)
 }
-//sort by first name
 
-function sortByFirst(studentA, studentB) {
-    if (studentA.firstName < studentB.firstName ) {
-        return -1;
-    } else {
-        return 1;
-    }
-}
-//sort by last name
-
-function sortByLast (studentA, studentB) {
-    if (studentA.lastName < studentB.lastName ) {
-        return -1;
-    } else {
-        return 1;
-    }
-}
-
-//sort by house
-function sortByHouse (studentA, studentB) {
-    if (studentA.house < studentB.house ) {
-        return -1;
-    } else {
-        return 1;
-    }
-}
 
 //display the data 
 
